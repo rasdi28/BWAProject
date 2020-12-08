32598d2dfe521ed26096709e7fd6728a5c126331
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2020-12-08 22:04:22 +0700

    membuat dinamic componen

--- projek/react1/src/component/YouTube/YouTubeComp.jsx
+++ projek/react1/src/component/YouTube/YouTubeComp.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './YoutubeComp.css';
 
-const YoutubeComp = () => {
+const YoutubeComp = (props) => {
     return (
         <div className = "YouTube-wrapper">
             <div className = "img-thumb">
                 <img src= " http://i3.ytimg.com/vi/ywDyFmPIj3Y/hqdefault.jpg" alt= ""/>
+                <p className = "time">{props.time}</p>
             </div>
             <p className="title"> Title here</p>
             <p  className="desc"> desc Here</p>
@@ -13,4 +14,7 @@ const YoutubeComp = () => {
     )
 }
 
+YoutubeComp.defaultProps = {
+    time : '00.00'
+}
 export default YoutubeComp;
--- projek/react1/src/container/Home/Home.jsx
+++ projek/react1/src/container/Home/Home.jsx
@@ -5,10 +5,12 @@ class Home extends Component {
     render(){
         return(
             <div>
+                <p> Youtube Component</p>
+                <hr/>
                 
-            <YouTubeComp/>
-            <YouTubeComp/>
-            <YouTubeComp/>
+            <YouTubeComp time= "7.4" title = "tutuorial React"/>
+            <YouTubeComp time= "7.7"/>
+            <YouTubeComp time= "7.9"/>
             <YouTubeComp/>
 
             </div>
